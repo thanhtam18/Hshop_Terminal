e4d1d96fb363967ff5f21c2eaf63eca3e6987dbb
thanhtam18 <tt867661396@gmail.com>
2024-05-06 22:48:48 +0700

    new version

---
+++ tool_upload/Upload_Uno.bat
@@ -0,0 +1,8 @@
+@echo off
+set avrdudePath=C:\Users\Tam\Desktop\Hshop\Hshop-Terminal\bin\Debug\tool_upload\bin\avrdude.exe
+set programmer=arduino
+set port=COM8
+set baudrate=115200
+set hexFile=C:\Users\Tam\Desktop\Hshop\HS000024_JoystickShield\firmware\Hshop_JoyStickShleld.ino_UNO.hex
+"%avrdudePath%" -C "C:\Users\Tam\Desktop\Hshop\Hshop-Terminal\bin\Debug\tool_upload\etc\avrdude.conf" -v -patmega328p -c %programmer% -P %port% -b %baudrate% -D -U flash:w:"%hexFile%":i
+pause
